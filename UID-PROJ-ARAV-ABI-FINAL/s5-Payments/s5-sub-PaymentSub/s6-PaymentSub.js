0cd06ee16b98b69f11a2cd9998b69c4608cab48d
Aravind Suresh <aravind.2002.suresh@gmail.com>
2021-07-18 22:54:06 +0530

    les get that dub

--- UID-PROJ-ARAV-ABI-FINAL/s5-Payments/s5-sub-PaymentSub/s6-PaymentSub.js
+++ UID-PROJ-ARAV-ABI-FINAL/s5-Payments/s5-sub-PaymentSub/s6-PaymentSub.js
@@ -4,7 +4,7 @@ function cvvCheck() {
     var cvv = document.getElementById('cvv').value;
     var tick = document.getElementById('tick');
 
-    //alert(cvv);
+    alert(cvv);
     if (cvv <= 999 && cvv >= 100) {
         tick.style.color = "green";
     } else {
