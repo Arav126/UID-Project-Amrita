7b75ddcf9ff3d0d463d155654ccefb16831eec23
Somil Bansal <somil.bansal.11@gmail.com>
2021-07-20 13:49:22 +0530

    sdfws

--- UID-PROJ-FINAL-REVIEW/z-global/global.js
+++ UID-PROJ-FINAL-REVIEW/z-global/global.js
@@ -1,35 +1,18 @@
 
-
+var g,gc;
 var i = 0;
-
-function enroute(lol){
-   
-
-    if(lol=="delhi"){
-        window.open("../s3-Enroute/s3-Delhi/delhi.html");
-    } else if(lol=="bombay") {
-        window.open('../s3-Enroute/s3-Bombay/BombayToHyderabad.html');
-    } else if(lol=="kolkata") {
-        window.open('../s3-Enroute/s3-Kolkata/kolkata.html');
-    } else if(lol=="chennai") {
-        window.open('../s3-Enroute/s3-Chennai/stage31.html');
-    } else if(lol=="gujarat") {
-        window.open('../s3-Enroute/s3-Gujarat/GujaratToHyderabad.html');
-    }
-
-    alert('end');
-}
 
 function mainLink() {
     
-    var g = document.whereForm.radios.value;
-    alert(g);
+    
+    gc=document.whereForm.radios.value.checked;
+    g = document.whereForm.radios.value;
+    
     
     ++i;
     if(i==3){
-        location.href="../4-DestinationsRedone/SubCoverAbi.html";
-        return g;
+        location.href=("../4-DestinationsRedone/SubCoverAbi.html");
+       
     }
-
 }
-var lol=mainLink();
+
